11846eafb8cc81e9416af1aa182772b643bb4497
Jeongho Nam <samchon.github@gmail.com>
2026-08-28 16:21:43 +0900

    feat(template): export production instruction tools

--- packages/template/src/index.ts
+++ packages/template/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./renderScaffold";
+export * from "./renderAutoMovieProductionRouter";
 export * from "./renderTemplate";
 export * from "./scaffoldFileSnapshot";
 export * from "./templateVersions";
 export * from "./writeFiles";
+export * from "./writeAutoMovieProductionInstructions";
